c92758079533212498806ddab37cdb550fb3da91
kyuia <2356173647@qq.com>
2025-04-15 21:51:49 +0800

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/111-checkpoint.py
+++
@@ -1,5 +0,0 @@
-# 临时调试代码：打印前5行数据的列数和首行数据
-import pandas as pd
-df = pd.read_csv("Datasets/BIOGRID-ALL-4.4.244.tab2.txt", sep='\t', nrows=5)
-print("Columns:", df.columns.tolist())
-print("First row data:", df.iloc[0].tolist())
